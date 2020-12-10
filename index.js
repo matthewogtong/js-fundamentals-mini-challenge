b528ebdc2100e6c34477c8daacd344f3a9e7cf3a
matthewogtong <ogtongm@gmail.com>
2020-12-10 12:48:09 -0500

    commit tree 335841a1be80f0d9704cb5b71b0857650aac82ff

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // ***** Question 1 *****
 // *** Uncomment the lines below to test
-// console.log("%cQuestion 1", "color: red") 
+console.log("%cQuestion 1", "color: red") 
 
-// console.log(counter) 
-// // => 2
-// console.log("%c----------", "color: red") 
+console.log(counter) 
+// => 2
+console.log("%c----------", "color: red") 
 
+let counter = 1
+
+counter = 2
 
 // ***** Question 2 *****
 // *** Uncomment the lines below to test
